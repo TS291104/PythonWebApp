8579c8fec9976ede8e49de90891f80a2a8eeb4a5
Alexandr Teplic <alexandr.teplic@katoennatie.com>
2019-04-15 11:46:49 +0300

    COMMIT

--- runserver.py
+++ runserver.py
@@ -1,14 +1,9 @@
-"""
-This script runs the FlaskWebProject2 application using a development server.
-"""
+from flask import Flask
+app = Flask(__name__)
 
-from os import environ
-from FlaskWebProject2 import app
-
-if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT)
+@app.route('/')
+def hello_world():
+	return 'Hello, World'
+	
+if __name__=='__main__':
+	app.run()
