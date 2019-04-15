a0e536f6ca24c62ba429dd24e9925c2ef0713758
Alexandr Teplic <alexandr.teplic@katoennatie.com>
2019-04-15 17:03:52 +0300

    COMMIT

--- FlaskWebProjec3/FlaskWebProjec3/views.py
+++ FlaskWebProjec3/FlaskWebProjec3/views.py
@@ -4,7 +4,7 @@ Routes and views for the flask application.
 
 from datetime import datetime
 from flask import render_template
-from FlaskWebProject4 import app
+from FlaskWebProjec3 import app
 
 @app.route('/')
 @app.route('/home')
--- FlaskWebProjec3/application.py
+++ FlaskWebProjec3/application.py
@@ -1,9 +1,9 @@
 """
-This script runs the FlaskWebProject4 application using a development server.
+This script runs the FlaskWebProjec3 application using a development server.
 """
 
 from os import environ
-from FlaskWebProject4 import app
+from FlaskWebProjec3 import app
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
